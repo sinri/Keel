37fbd735c86d3847fc2320cde04451ed81465314
Sinri Edogawa <ljni@leqee.com>
2022-03-09 10:16:55 +0800

    Changes

--- src/main/java/io/github/sinri/keel/mysql/statement/AbstractReadStatement.java
+++ src/main/java/io/github/sinri/keel/mysql/statement/AbstractReadStatement.java
@@ -1,9 +1,12 @@
 package io.github.sinri.keel.mysql.statement;
 
 import io.github.sinri.keel.mysql.MySQLExecutor;
+import io.github.sinri.keel.mysql.exception.KeelSQLResultRowIndexError;
 import io.github.sinri.keel.mysql.jdbc.KeelJDBCForMySQL;
 import io.github.sinri.keel.mysql.matrix.AbstractTableRow;
 import io.github.sinri.keel.mysql.matrix.ResultMatrix;
+import io.vertx.core.Future;
+import io.vertx.sqlclient.data.Numeric;
 
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -25,5 +28,107 @@ public abstract class AbstractReadStatement extends AbstractStatement {
 
     public <T extends AbstractTableRow> MySQLExecutor<List<T>> getTableRowListFetcher(Class<T> classOfTableRow) {
         return AbstractTableRow.buildTableRowListFetcher(this, classOfTableRow);
+    }
+
+    /**
+     * @param fieldName
+     * @return
+     * @since 1.13
+     */
+    public MySQLExecutor<List<Long>> getLongColumnFetcher(String fieldName) {
+        return MySQLExecutor.build(
+                sqlConnection -> this.execute(sqlConnection)
+                        .compose(resultMatrix -> Future.succeededFuture(resultMatrix.getOneColumnAsLong(fieldName))),
+                statement -> this.blockedExecute(statement).getOneColumnAsLong(fieldName)
+        );
+    }
+
+    /**
+     * @param fieldName
+     * @return
+     * @since 1.13
+     */
+    public MySQLExecutor<List<Integer>> getIntegerColumnFetcher(String fieldName) {
+        return MySQLExecutor.build(
+                sqlConnection -> this.execute(sqlConnection)
+                        .compose(resultMatrix -> Future.succeededFuture(resultMatrix.getOneColumnAsInteger(fieldName))),
+                statement -> this.blockedExecute(statement).getOneColumnAsInteger(fieldName)
+        );
+    }
+
+    /**
+     * @param fieldName
+     * @return
+     * @since 1.13
+     */
+    public MySQLExecutor<List<Numeric>> getNumberColumnFetcher(String fieldName) {
+        return MySQLExecutor.build(
+                sqlConnection -> this.execute(sqlConnection)
+                        .compose(resultMatrix -> Future.succeededFuture(resultMatrix.getOneColumnAsNumeric(fieldName))),
+                statement -> this.blockedExecute(statement).getOneColumnAsNumeric(fieldName)
+        );
+    }
+
+    /**
+     * @param fieldName
+     * @return
+     * @since 1.13
+     */
+    public MySQLExecutor<List<String>> getStringColumnFetcher(String fieldName) {
+        return MySQLExecutor.build(
+                sqlConnection -> this.execute(sqlConnection)
+                        .compose(resultMatrix -> Future.succeededFuture(resultMatrix.getOneColumnAsString(fieldName))),
+                statement -> this.blockedExecute(statement).getOneColumnAsString(fieldName)
+        );
+    }
+
+    /**
+     * @param fieldName
+     * @return
+     * @since 1.13
+     */
+    public MySQLExecutor<String> getStringCellFetcher(String fieldName) {
+        return MySQLExecutor.build(
+                sqlConnection -> this.execute(sqlConnection)
+                        .compose(resultMatrix -> {
+                            try {
+                                return Future.succeededFuture(resultMatrix.getOneColumnOfFirstRowAsString(fieldName));
+                            } catch (KeelSQLResultRowIndexError e) {
+                                return null;
+                            }
+                        }),
+                statement -> {
+                    try {
+                        return this.blockedExecute(statement).getOneColumnOfFirstRowAsString(fieldName);
+                    } catch (KeelSQLResultRowIndexError e) {
+                        return null;
+                    }
+                }
+        );
+    }
+
+    /**
+     * @param fieldName
+     * @return
+     * @since 1.13
+     */
+    public MySQLExecutor<Numeric> getNumericCellFetcher(String fieldName) {
+        return MySQLExecutor.build(
+                sqlConnection -> this.execute(sqlConnection)
+                        .compose(resultMatrix -> {
+                            try {
+                                return Future.succeededFuture(resultMatrix.getOneColumnOfFirstRowAsNumeric(fieldName));
+                            } catch (KeelSQLResultRowIndexError e) {
+                                return null;
+                            }
+                        }),
+                statement -> {
+                    try {
+                        return this.blockedExecute(statement).getOneColumnOfFirstRowAsNumeric(fieldName);
+                    } catch (KeelSQLResultRowIndexError e) {
+                        return null;
+                    }
+                }
+        );
     }
 }
---
+++ src/test/java/io/github/sinri/keel/test/helper/yaml/YAMLTest.java
@@ -0,0 +1,38 @@
+package io.github.sinri.keel.test.helper.yaml;
+
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLParser;
+import io.github.sinri.keel.core.KeelHelper;
+
+import java.io.IOException;
+
+public class YAMLTest {
+    public static void main(String[] args) {
+        byte[] bytes = new byte[0];
+        try {
+            bytes = KeelHelper.readFileAsByteArray("test.yml", true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        YAMLFactory yamlFactory = new YAMLFactory();
+        //var mapper = new ObjectMapper(yamlFactory);
+        //mapper.findAndRegisterModules();
+
+        try {
+            YAMLParser parser = yamlFactory.createParser(bytes);
+            while (parser.nextFieldName() != null) {
+
+//            }
+//            while (parser.nextToken() != null) {
+                System.out.println(parser.currentToken());
+                System.out.println("\t" + parser.currentName() + " = " + parser.currentValue());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+//        return mapper.readValue(new String(bytes, StandardCharsets.UTF_8), classOfT);
+    }
+}
