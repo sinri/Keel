956a9a78ec76b6ec6c164c3337f37cecdf9ab737
Sinri Edogawa <ljni@leqee.com>
2022-07-08 16:49:00 +0800

    2.8-SNAPSHOT JsonifiableEntity extends there interfaces

--- src/main/java/io/github/sinri/keel/core/json/JsonifiableEntity.java
+++ src/main/java/io/github/sinri/keel/core/json/JsonifiableEntity.java
@@ -1,16 +1,24 @@
 package io.github.sinri.keel.core.json;
 
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.json.pointer.JsonPointer;
+import io.vertx.core.shareddata.ClusterSerializable;
+import io.vertx.core.shareddata.Shareable;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.function.Function;
 
 /**
  * @since 1.14
+ * @since 2.8 ClusterSerializable: Safe with EventBus Messaging.
+ * @since 2.8 Shareable: allows you to put into a LocalMap.
+ * @since 2.8 Iterable: you can run forEach with it.
  */
-public interface JsonifiableEntity<E> {
+public interface JsonifiableEntity<E> extends ClusterSerializable, Iterable<Map.Entry<String, Object>>, Shareable {
     JsonObject toJsonObject();
 
     E reloadDataFromJsonObject(JsonObject jsonObject);
@@ -143,9 +151,50 @@ public interface JsonifiableEntity<E> {
         JsonObject jsonObject = readJsonObject(args);
         try {
             return bClass.getConstructor(JsonObject.class).newInstance(jsonObject);
-        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException |
+                 NoSuchMethodException e) {
             //e.printStackTrace();
             return null;
         }
+    }
+
+    /**
+     * @since 2.8
+     */
+    default Buffer toBuffer() {
+        return toJsonObject().toBuffer();
+    }
+
+    /**
+     * @since 2.8
+     */
+    default void fromBuffer(Buffer buffer) {
+        this.reloadDataFromJsonObject(new JsonObject(buffer));
+    }
+
+    /**
+     * @since 2.8
+     */
+    default void writeToBuffer(Buffer buffer) {
+        JsonObject jsonObject = this.toJsonObject();
+        if (jsonObject == null) {
+            jsonObject = new JsonObject();
+        }
+        jsonObject.writeToBuffer(buffer);
+    }
+
+    /**
+     * @since 2.8
+     */
+    default int readFromBuffer(int pos, Buffer buffer) {
+        JsonObject jsonObject = new JsonObject();
+        int i = jsonObject.readFromBuffer(pos, buffer);
+        this.reloadDataFromJsonObject(jsonObject);
+        return i;
+    }
+
+    @Override
+    default Iterator<Map.Entry<String, Object>> iterator() {
+        return toJsonObject().iterator();
     }
 }
--- src/main/java/io/github/sinri/keel/core/json/SimpleJsonifiableEntity.java
+++ src/main/java/io/github/sinri/keel/core/json/SimpleJsonifiableEntity.java
@@ -2,14 +2,19 @@ package io.github.sinri.keel.core.json;
 
 import io.vertx.core.json.JsonObject;
 
+import java.util.Objects;
+
 /**
  * @since 2.7
  */
 public class SimpleJsonifiableEntity implements JsonifiableEntity<SimpleJsonifiableEntity> {
-    private JsonObject jsonObject;
+    protected JsonObject jsonObject;
 
+    /**
+     * @since 2.8 jsonObject initialized
+     */
     public SimpleJsonifiableEntity() {
-
+        this.jsonObject = new JsonObject();
     }
 
     public SimpleJsonifiableEntity(JsonObject jsonObject) {
@@ -25,5 +30,21 @@ public class SimpleJsonifiableEntity implements JsonifiableEntity<SimpleJsonifia
     public SimpleJsonifiableEntity reloadDataFromJsonObject(JsonObject jsonObject) {
         this.jsonObject = jsonObject;
         return this;
+    }
+
+    /**
+     * @since 2.8
+     */
+    @Override
+    public String toString() {
+        return Objects.requireNonNullElse(toJsonObject(), new JsonObject()).toString();
+    }
+
+    @Override
+    public SimpleJsonifiableEntity copy() {
+        SimpleJsonifiableEntity copied = new SimpleJsonifiableEntity();
+        JsonObject copiedJsonObject = this.toJsonObject().copy();
+        copied.reloadDataFromJsonObject(copiedJsonObject);
+        return copied;
     }
 }
