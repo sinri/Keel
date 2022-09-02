5e9a6af17ebed9798e9bd8422e9108526ed9a6fd
Sinri Edogawa <ljni@leqee.com>
2022-09-02 13:44:07 +0800

    用 org.bouncycastle 实现AES PKCS7Padding 的代码保留，但Keel中不再引用BC。

--- src/main/java/io/github/sinri/keel/core/helper/encryption/aes/KeelAesUsingPkcs7Padding.java
+++ src/main/java/io/github/sinri/keel/core/helper/encryption/aes/KeelAesUsingPkcs7Padding.java
@@ -1,16 +1,40 @@
 package io.github.sinri.keel.core.helper.encryption.aes;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-
+import java.lang.reflect.InvocationTargetException;
+import java.security.Provider;
 import java.security.Security;
 
 /**
  * @since 2.8
  */
 abstract public class KeelAesUsingPkcs7Padding extends KeelAesBase {
-    static {
-        //如果是PKCS7Padding填充方式，则必须加上下面这行
-        Security.addProvider(new BouncyCastleProvider());
+//    static {
+//        //如果是PKCS7Padding填充方式，则必须加上下面这行
+//        Security.addProvider(new BouncyCastleProvider());
+//    }
+
+    /**
+     * 直接使用static代码块引用 org.bouncycastle.jce.provider.BouncyCastleProvider 会引起shade问题：
+     * `Invalid signature file digest for Manifest main attributes`。
+     * 当需要使用BC支持时，应当在POM引入相应的依赖，在MAIN的初始化中提前调用此方法
+     */
+    public static void requireBouncyCastleProvider() {
+        /*
+        <dependency>
+            <groupId>org.bouncycastle</groupId>
+            <artifactId>bcprov-jdk15on</artifactId>
+            <version>1.70</version>
+        </dependency>
+         */
+
+        try {
+            Class<?> providerClass = Class.forName("org.bouncycastle.jce.provider.BouncyCastleProvider");
+            Provider provider = (Provider) providerClass.getConstructor().newInstance();
+            Security.addProvider(provider);
+        } catch (ClassNotFoundException | InvocationTargetException | InstantiationException | IllegalAccessException |
+                 NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
