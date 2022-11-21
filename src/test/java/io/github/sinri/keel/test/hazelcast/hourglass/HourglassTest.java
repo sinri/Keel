2512874d81a871bb96924b762b3367d76d57fef1
Sinri Edogawa <ljni@leqee.com>
2022-11-21 13:24:07 +0800

    Keel 2.9.3-SNAPSHOT Hourglass

--- src/test/java/io/github/sinri/keel/test/hazelcast/hourglass/HourglassTest.java
+++ src/test/java/io/github/sinri/keel/test/hazelcast/hourglass/HourglassTest.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class HourglassTest {
     public static void testPure() {
         KeelPureHourglass keelPureHourglass = new KeelPureHourglass(HourglassTest.class.getSimpleName());
-        keelPureHourglass.setLogger(Keel.outputLogger());
+        //keelPureHourglass.setLogger(Keel.outputLogger());
         keelPureHourglass.setInterval(60_000L).setHandler(now -> {
             String dateExpression = Keel.helpers().datetime().getDateExpression(new Date(now), "yyyy-MM-dd HH:mm");
             keelPureHourglass.getLogger().info("RUN " + dateExpression + " triggered : " + now);
