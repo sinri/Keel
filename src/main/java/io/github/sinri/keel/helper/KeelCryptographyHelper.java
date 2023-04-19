0c24982f2244b029c6d7da2cabcc5d93709738b5
Sinri Edogawa <ljni@leqee.com>
2023-04-19 15:57:12 +0800

    RSA

--- src/main/java/io/github/sinri/keel/helper/KeelCryptographyHelper.java
+++ src/main/java/io/github/sinri/keel/helper/KeelCryptographyHelper.java
@@ -1,6 +1,7 @@
 package io.github.sinri.keel.helper;
 
 import io.github.sinri.keel.helper.encryption.aes.KeelAes;
+import io.github.sinri.keel.helper.encryption.rsa.KeelRSA;
 
 /**
  * @since 2.8
@@ -18,5 +19,12 @@ public class KeelCryptographyHelper {
 
     public KeelAes aes(KeelAes.SupportedCipherAlgorithm cipherAlgorithm, String key) {
         return KeelAes.create(cipherAlgorithm, key);
+    }
+
+    /**
+     * @since 3.0.1
+     */
+    public KeelRSA rsa() {
+        return new KeelRSA();
     }
 }
