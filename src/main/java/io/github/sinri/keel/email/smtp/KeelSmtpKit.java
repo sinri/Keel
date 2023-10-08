53eca5d06740533b8c88d5f5b6ae8d95949e7178
Sinri Edogawa <ljni@leqee.com>
2023-10-08 10:54:04 +0800

    3.0.6-SNAPSHOT refine SMTP
    Signed-off-by: Sinri Edogawa <ljni@leqee.com>

--- src/main/java/io/github/sinri/keel/email/smtp/KeelSmtpKit.java
+++ src/main/java/io/github/sinri/keel/email/smtp/KeelSmtpKit.java
@@ -9,59 +9,70 @@ import io.vertx.ext.mail.MailMessage;
 import io.vertx.ext.mail.MailResult;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * @since 1.10
+ * @since 3.0.6 changed a lot
  */
 public class KeelSmtpKit {
-    private final String smtpName;
     private final MailConfig mailConfig;
     private final MailClient mailClient;
 
-    public KeelSmtpKit(String smtpName, boolean shared) {
-        this.smtpName = smtpName;
-        this.mailConfig = buildMailConfig(Keel.getConfiguration(), smtpName);
-        if (shared) {
-            this.mailClient = MailClient.createShared(Keel.getVertx(), this.mailConfig, smtpName);
+    /**
+     * @since 3.0.6
+     */
+    public KeelSmtpKit(MailConfig mailConfig, String poolName) {
+        this.mailConfig = mailConfig;
+        if (poolName != null) {
+            this.mailClient = MailClient.createShared(Keel.getVertx(), this.mailConfig, poolName);
         } else {
             this.mailClient = MailClient.create(Keel.getVertx(), this.mailConfig);
         }
     }
 
+    public KeelSmtpKit(String smtpName, boolean shared) {
+        this(buildMailConfig(smtpName), shared ? Objects.requireNonNull(smtpName) : null);
+    }
+
     public KeelSmtpKit(String smtpName) {
-        this.smtpName = smtpName;
-        this.mailConfig = buildMailConfig(Keel.getConfiguration(), smtpName);
-        this.mailClient = MailClient.create(Keel.getVertx(), this.mailConfig);
+        this(smtpName, true);
     }
 
     public KeelSmtpKit() {
-        this.smtpName = Keel.getConfiguration().readString("email", "smtp", "default_smtp_name");
-        this.mailConfig = buildMailConfig(Keel.getConfiguration(), smtpName);
-        this.mailClient = MailClient.create(Keel.getVertx(), this.mailConfig);
+        this(
+                Objects.requireNonNull(
+                        Keel.getConfiguration().readString("email", "smtp", "default_smtp_name"),
+                        "email.smtp.default_smtp_name is not configured"
+                )
+        );
     }
 
-    private static MailConfig buildMailConfig(KeelConfiguration keelConfiguration, String smtpName) {
-        KeelConfiguration smtpConfiguration = keelConfiguration.extract("email", "smtp", smtpName);
-        if (smtpConfiguration != null) {
-            return new MailConfig(smtpConfiguration.toJsonObject());
-        } else {
-            return new MailConfig();
-        }
-    }
+    /**
+     * As of 3.0.6, only five property keys supported.
+     */
+    private static MailConfig buildMailConfig(String smtpName) {
+        KeelConfiguration smtpConfiguration = Keel.getConfiguration().extract("email", "smtp", smtpName);
 
-    public String getSmtpName() {
-        return smtpName;
+        var mailConfig = new MailConfig();
+        mailConfig.setHostname(smtpConfiguration.readString("hostname"));
+        mailConfig.setPort(Objects.requireNonNull(smtpConfiguration.readAsInteger("port")));
+        mailConfig.setUsername(smtpConfiguration.readString("username"));
+        mailConfig.setPassword(smtpConfiguration.readString("password"));
+        mailConfig.setSsl("ON".equals(smtpConfiguration.readString("ssl")));
+
+        return mailConfig;
     }
 
     public MailClient getMailClient() {
         return mailClient;
     }
 
-    public void close() {
+    public Future<Void> close() {
         if (null != mailClient) {
-            mailClient.close();
-            System.out.println("KeelSmtpKit closed client " + this.smtpName);
+            return mailClient.close();
         }
+        return Future.succeededFuture();
     }
 
     public Future<MailResult> quickSendTextMail(
@@ -90,5 +101,12 @@ public class KeelSmtpKit {
         message.setHtml(htmlContent);
 
         return this.mailClient.sendMail(message);
+    }
+
+    /**
+     * @since 3.0.6
+     */
+    public MailConfig getMailConfig() {
+        return mailConfig;
     }
 }
