d821e73d9bbcc2b026baa7d61428b36a1892f82a
Sinri Edogawa <ljni@leqee.com>
2024-01-11 10:45:41 +0800

    3.1.0-SNAPSHOT ValueBox Expire able
    Signed-off-by: Sinri Edogawa <ljni@leqee.com>

--- src/main/java/io/github/sinri/keel/core/ValueBox.java
+++ src/main/java/io/github/sinri/keel/core/ValueBox.java
@@ -12,23 +12,46 @@ import javax.annotation.Nullable;
 public class ValueBox<T> {
     private T value;
     private boolean valueAlreadySet;
+    /**
+     * @since 3.1.0
+     * When expire is equal or less than zero, never expire;
+     * Or as the milliseconds to reserve the value.
+     */
+    private long expire = 0;
 
     public ValueBox() {
         this.clear();
     }
 
     public ValueBox(@Nullable T value) {
-        this.setValue(value);
+        this(value, 0);
+    }
+
+    public ValueBox(@Nullable T value, long lifetime) {
+        this.setValue(value, lifetime);
     }
 
     public ValueBox<T> clear() {
         this.value = null;
         this.valueAlreadySet = false;
+        this.expire = 0;
         return this;
     }
 
+    /**
+     * If the value is already set, and, not expired if lifetime declared.
+     * When checked, if the value set but expired, it would be cleaned.
+     */
     public boolean isValueAlreadySet() {
-        return valueAlreadySet;
+        if (!valueAlreadySet) return false;
+        if (expire <= 0) {
+            return true;
+        }
+        if (expire > System.currentTimeMillis()) {
+            return true;
+        }
+        this.clear();
+        return false;
     }
 
     public T getValue() {
@@ -36,9 +59,23 @@ public class ValueBox<T> {
         else throw new IllegalStateException("Value is not set yet");
     }
 
-    public ValueBox<T> setValue(@Nullable T value) {
+    /**
+     * @param fallbackForInvalid it would be return when the value is not set yet.
+     * @since 3.1.0
+     */
+    public T getValueOrElse(T fallbackForInvalid) {
+        if (isValueAlreadySet()) return value;
+        else return fallbackForInvalid;
+    }
+
+    public ValueBox<T> setValue(@Nullable T value, long lifetime) {
         this.value = value;
         this.valueAlreadySet = true;
+        if (lifetime > 0) {
+            this.expire = System.currentTimeMillis() + lifetime;
+        } else {
+            this.expire = 0;
+        }
         return this;
     }
 
