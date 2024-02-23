6b5937df116112384010d579b0d60a090c9bd8e3
Sinri Edogawa <ljni@leqee.com>
2024-02-23 16:24:54 +0800

    3.2.0-SNAPSHOT + remaster KeelMetricRecord to use KeelIssueRecord
    Signed-off-by: Sinri Edogawa <ljni@leqee.com>

--- src/main/java/io/github/sinri/keel/logger/metric/KeelMetricRecord.java
+++ src/main/java/io/github/sinri/keel/logger/metric/KeelMetricRecord.java
@@ -1,77 +1,70 @@
 package io.github.sinri.keel.logger.metric;
 
 import io.github.sinri.keel.core.TechnicalPreview;
-import io.github.sinri.keel.core.json.SimpleJsonifiableEntity;
+import io.github.sinri.keel.logger.issue.record.BaseIssueRecord;
 import io.vertx.core.json.JsonObject;
 
+import javax.annotation.Nonnull;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
 /**
  * @since 3.1.9 Technical Preview
+ * @since 3.2.0 extends BaseIssueRecord
+ * It is allowed to override this class, for fixed topic and metric.
  */
 @TechnicalPreview(since = "3.1.9")
-public class KeelMetricRecord extends SimpleJsonifiableEntity {
-    public KeelMetricRecord(String metricName, double value) {
-        this.jsonObject = new JsonObject()
-                .put("timestamp", System.currentTimeMillis())
-                .put("labels", new JsonObject());
-        this.metricName(metricName);
-        this.value(value);
+public class KeelMetricRecord extends BaseIssueRecord<KeelMetricRecord> {
+    private final @Nonnull String topic;
+    private final @Nonnull Map<String, String> labelMap = new HashMap<>();
+    private final @Nonnull String metricName;
+    private final double value;
+
+    public KeelMetricRecord(@Nonnull String topic, @Nonnull String metricName, double value) {
+        super();
+        this.topic = topic;
+        this.metricName = metricName;
+        this.value = value;
     }
 
-    public long timestamp() {
-        return Objects.requireNonNull(readLong("timestamp"));
+    public JsonObject toJsonObject() {
+        JsonObject labelObject = new JsonObject();
+        labelMap.forEach(labelObject::put);
+        return new JsonObject()
+                .put("timestamp", timestamp())
+                .put("labels", labelObject)
+                .put("metric_name", metricName)
+                .put("value", value);
     }
 
-    public KeelMetricRecord timestamp(long timestamp) {
-        this.jsonObject.put("timestamp", timestamp);
-        return this;
-    }
 
-    public String topic() {
-        return Objects.requireNonNull(readString("topic"));
-    }
-
-    public KeelMetricRecord topic(String topic) {
-        this.jsonObject.put("topic", topic);
-        return this;
-    }
-
+    @Nonnull
     public String metricName() {
-        return Objects.requireNonNull(readString("metric_name"));
-    }
-
-    public KeelMetricRecord metricName(String metricName) {
-        this.jsonObject.put("metric_name", metricName);
-        return this;
+        return metricName;
     }
 
     public double value() {
-        return Objects.requireNonNull(readDouble("value"));
-    }
-
-    public KeelMetricRecord value(double value) {
-        this.jsonObject.put("value", value);
-        return this;
+        return value;
     }
 
     public Map<String, String> labels() {
-        var x = readJsonObject("labels");
-        if (x == null) {
-            return Map.of();
-        }
-        Map<String, String> map = new HashMap<>();
-        x.forEach(entry -> {
-            map.put(entry.getKey(), String.valueOf(entry.getValue()));
-        });
-        return map;
+        return labelMap;
     }
 
     public KeelMetricRecord label(String name, String value) {
-        this.jsonObject.getJsonObject("labels")
-                .put(name, value);
+        this.labelMap.put(name, value);
         return this;
+    }
+
+    @Nonnull
+    @Override
+    public KeelMetricRecord getImplementation() {
+        return this;
+    }
+
+    @Nonnull
+    @Override
+    public String topic() {
+        return topic;
     }
 }
