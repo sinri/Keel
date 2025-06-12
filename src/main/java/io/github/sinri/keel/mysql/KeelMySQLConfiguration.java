8ff49feb76a519def394deaa0e274e54d789053f
sinri <e.joshua.s.e@gmail.com>
2025-06-12 17:02:08 +0800

    3.2.22
    Add QuickQueryTest for MySQL instant query methods and bump version to 3.2.22
    
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

--- src/main/java/io/github/sinri/keel/mysql/KeelMySQLConfiguration.java
+++ src/main/java/io/github/sinri/keel/mysql/KeelMySQLConfiguration.java
@@ -47,9 +47,9 @@ public class KeelMySQLConfiguration extends KeelConfigElement {
         MySQLConnectOptions mySQLConnectOptions = new MySQLConnectOptions()
                 .setUseAffectedRows(true);
         mySQLConnectOptions.setHost(getHost())
-                .setPort(getPort())
-                .setUser(getUsername())
-                .setPassword(getPassword());
+                           .setPort(getPort())
+                           .setUser(getUsername())
+                           .setPassword(getPassword());
         String charset = getCharset();
         if (charset != null) mySQLConnectOptions.setCharset(charset);
         String schema = getDatabase();
@@ -150,7 +150,8 @@ public class KeelMySQLConfiguration extends KeelConfigElement {
      * If the time is exceeded without a connection available, an exception is provided.
      * TimeUnit would be set by `setConnectionTimeoutUnit`
      *
-     * @see <a href="https://vertx.io/docs/apidocs/io/vertx/sqlclient/PoolOptions.html#setConnectionTimeout-int-">...</a>
+     * @see <a
+     *         href="https://vertx.io/docs/apidocs/io/vertx/sqlclient/PoolOptions.html#setConnectionTimeout-int-">...</a>
      */
     public Integer getPoolConnectionTimeout() {
         KeelConfigElement keelConfigElement = extract("poolConnectionTimeout");
@@ -162,9 +163,9 @@ public class KeelMySQLConfiguration extends KeelConfigElement {
 
     /**
      * @since 3.0.9
-     * You can share a pool between multiple verticles or instances of the same verticle.
-     * Such pool should be created outside a verticle otherwise it will be closed when the verticle
-     * that created it is undeployed.
+     *         You can share a pool between multiple verticles or instances of the same verticle.
+     *         Such pool should be created outside a verticle otherwise it will be closed when the verticle
+     *         that created it is undeployed.
      */
     public boolean getPoolShared() {
         return readBoolean("poolShared", true);
@@ -181,17 +182,17 @@ public class KeelMySQLConfiguration extends KeelConfigElement {
     @TechnicalPreview(since = "3.1.6")
     public Future<ResultMatrix> instantQuery(String sql) {
         var sqlClient = MySQLBuilder.client()
-                .with(this.getPoolOptions())
-                .connectingTo(this.getConnectOptions())
-                .using(Keel.getVertx())
-                .build();
+                                    .with(this.getPoolOptions())
+                                    .connectingTo(this.getConnectOptions())
+                                    .using(Keel.getVertx())
+                                    .build();
         // System.out.println("sqlClient: "+sqlClient.getClass()); // class io.vertx.mysqlclient.impl.MySQLPoolImpl
         return Future.succeededFuture()
-                .compose(v -> sqlClient.preparedQuery(sql).execute()
-                        .compose(rows -> {
-                            return Future.succeededFuture(ResultMatrix.create(rows));
-                        }))
-                .andThen(ar -> sqlClient.close());
+                     .compose(v -> sqlClient.preparedQuery(sql).execute()
+                                            .compose(rows -> {
+                                                return Future.succeededFuture(ResultMatrix.create(rows));
+                                            }))
+                     .andThen(ar -> sqlClient.close());
     }
 
     /**
@@ -206,34 +207,36 @@ public class KeelMySQLConfiguration extends KeelConfigElement {
     @TechnicalPreview(since = "3.2.21")
     public Future<Void> instantQueryForStream(String sql, int readWindowSize, Function<RowSet<Row>, Future<Void>> readWindowFunction) {
         return Future.succeededFuture()
-                .compose(v -> {
-                    Pool pool = MySQLBuilder.pool()
-                            .with(this.getPoolOptions())
-                            .connectingTo(this.getConnectOptions())
-                            .using(Keel.getVertx())
-                            .build();
-                    return Future.succeededFuture(pool);
-                })
-                .compose(pool -> {
-                    return pool.getConnection()
-                            .compose(sqlConnection -> {
-                                return sqlConnection.prepare(sql)
-                                        .compose(preparedStatement -> {
-                                            Cursor cursor = preparedStatement.cursor();
-
-                                            return KeelAsyncKit.repeatedlyCall(routineResult -> {
-                                                        if (!cursor.hasMore()) {
-                                                            routineResult.stop();
-                                                            return Future.succeededFuture();
-                                                        }
-
-                                                                   return cursor.read(readWindowSize).compose(readWindowFunction);
-                                                    })
-                                                    .eventually(() -> cursor.close());
-                                        })
-                                        .eventually(() -> sqlConnection.close());
-                            })
-                            .eventually(() -> pool.close());
-                });
+                     .compose(v -> {
+                         Pool pool = MySQLBuilder.pool()
+                                                 .with(this.getPoolOptions())
+                                                 .connectingTo(this.getConnectOptions())
+                                                 .using(Keel.getVertx())
+                                                 .build();
+                         return Future.succeededFuture(pool);
+                     })
+                     .compose(pool -> {
+                         return pool.getConnection()
+                                    .compose(sqlConnection -> {
+                                        return sqlConnection.prepare(sql)
+                                                            .compose(preparedStatement -> {
+                                                                Cursor cursor = preparedStatement.cursor();
+                                                                return KeelAsyncKit.repeatedlyCall(routineResult -> {
+                                                                                       return cursor.read(readWindowSize)
+                                                                                                    .compose(readWindowFunction)
+                                                                                                    .compose(v -> {
+                                                                                                        if (!cursor.hasMore()) {
+                                                                                                            routineResult.stop();
+                                                                                                            return Future.succeededFuture();
+                                                                                                        }
+                                                                                                        return Future.succeededFuture();
+                                                                                                    });
+                                                                                   })
+                                                                                   .eventually(() -> cursor.close());
+                                                            })
+                                                            .eventually(() -> sqlConnection.close());
+                                    })
+                                    .eventually(() -> pool.close());
+                     });
     }
 }
---
+++ src/test/java/io/github/sinri/keel/test/lab/mysql/QuickQueryTest.java
@@ -0,0 +1,56 @@
+package io.github.sinri.keel.test.lab.mysql;
+
+import io.github.sinri.keel.mysql.DynamicNamedMySQLConnection;
+import io.github.sinri.keel.mysql.KeelMySQLDataSourceProvider;
+import io.github.sinri.keel.mysql.NamedMySQLDataSource;
+import io.github.sinri.keel.tesuto.KeelTest;
+import io.github.sinri.keel.tesuto.TestUnit;
+import io.vertx.core.Future;
+import io.vertx.sqlclient.Row;
+import io.vertx.sqlclient.RowIterator;
+
+import javax.annotation.Nonnull;
+
+import static io.github.sinri.keel.facade.KeelInstance.Keel;
+
+public class QuickQueryTest extends KeelTest {
+    @Nonnull
+    @Override
+    protected Future<Void> starting() {
+        return super.starting()
+                    .compose(v -> {
+                        Keel.getConfiguration().loadPropertiesFile("config.properties");
+                        return Future.succeededFuture();
+                    });
+    }
+
+    @TestUnit
+    public Future<Void> test1() {
+        NamedMySQLDataSource<DynamicNamedMySQLConnection> cs = KeelMySQLDataSourceProvider.initializeDynamicNamedMySQLDataSource("cs");
+        return cs.getConfiguration().instantQueryForStream(
+                         "select * from cornerstone.file where file_id<0 limit 10",
+                         1 + 1,
+                         rowList -> {
+                             RowIterator<Row> iterator = rowList.iterator();
+                             while (iterator.hasNext()) {
+                                 Row next = iterator.next();
+                                 getLogger().info("row: " + next.getLong("file_id"));
+                             }
+                             return Future.succeededFuture();
+                         })
+                 .compose(v -> {
+                     getLogger().info("fin");
+                     return Future.succeededFuture();
+                 });
+    }
+
+    @TestUnit
+    public Future<Void> test2() {
+        NamedMySQLDataSource<DynamicNamedMySQLConnection> cs = KeelMySQLDataSourceProvider.initializeDynamicNamedMySQLDataSource("cs");
+        return cs.getConfiguration().instantQuery("select * from cornerstone.file limit 10")
+                 .compose(resultMatrix -> {
+                     getLogger().info("resultMatrix: " + resultMatrix.toJsonArray());
+                     return Future.succeededFuture();
+                 });
+    }
+}
